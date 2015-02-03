d7fde56357353634f5a6c8a566233db18d2465d5
Richard Collin <richard@collin.me.uk>
2015-02-03 15:34:41 +0000

    a

--- thing.js
+++ thing.js
@@ -1,33 +1,14 @@
 var util = require('util');
 var events = require('events');
 
-function Thing(_name, _states, _actions) {
+function Thing(_name) {
    this.name = 'thing:' + _name;
-   this.states = _states;
-   this.actions = _actions;
+   this.states = {};
+   this.actions = {};
 
    events.EventEmitter.call(this);
    var that = this;
 
-   var stateKeys = Object.keys(this.states);
-
-   stateKeys.forEach(function(state) {
-      that.states[state].setThing(that);  
-
-      that.states[state].on('active', function (sourceName) {
-         console.log(that.name + ' state ' + sourceName + ' has become active');
-      });
-
-      that.states[state].on('inactive', function (sourceName) {
-         console.log(that.name + ' state ' + sourceName + ' has become inactive');
-      });
-   });
-
-   //console.log(util.inspect(states, false, null));
-
-   var actionKeys = Object.keys(this.actions);
-
-   actionKeys.forEach(function(action) {
       //actions[actions].setThing(this);  
 
       that.actions[action].on('activated', function (sourceName) {
@@ -41,5 +22,31 @@ function Thing(_name, _states, _actions) {
 }
 
 util.inherits(Thing, events.EventEmitter);
+
+Thing.prototype.addState = function(_state) {
+   this.states[_state.name] = _state;
+
+   _state.on('active', function (sourceName) {
+      console.log(this.name + ': state ' + sourceName + ' has become active');
+   });
+
+   _state.on('inactive', function (sourceName) {
+      console.log(this.name + ': state ' + sourceName + ' has become inactive');
+   });
+   console.log(this.name + ': ' + _state.name + ' associated!');
+}
+
+Thing.prototype.addAction = function(_action) {
+   this.actions[_action.name] = _action;
+
+   _action.on('activated', function (sourceName) {
+      console.log(this.name + ': action ' + sourceName + ' has been activated');
+   });
+
+   _action.on('deactivated', function (sourceName) {
+      console.log(that.name + ': action ' + sourceName + ' has been deactivated');
+   });
+   console.log(this.name + ': ' + _action.name + ' associated!');
+}
 
 module.exports = exports = Thing;
