3a33cd85624b76dd3d43fcc205a9c7e16654c77e
Richard Collin <richard@collin.me.uk>
2015-02-21 18:14:50 +0000

    bugs

--- src/logicactivator.js
+++ src/logicactivator.js
@@ -9,66 +9,63 @@ function LogicActivator(_name, _sources, _timeout, _invert) {
 
    var that = this;
 
-   var index = 0;
-   _sources.forEach(function(_source) {
+   _sources.forEach(function(_source, index) {
       that.inputs.push( { source : _source, active : false });
 
       that.inputs[index].source.on('activate', function (sourceName) {
-         sourceActive(sourceName);
+         that.sourceIsActive(sourceName);
       });
 
       that.inputs[index].source.on('active', function (sourceName) {
-         sourceActive(sourceName);
-      });
-
-      var sourceActive = function(sourceName) {
-         console.log(that.name + ': Input source ' + sourceName + ' active!');
-         
-         // find the input in my array
-         items = that.inputs.filter(function(item) {
-           return (item.source.name == sourceName);
-         });
-
-         // set source input to active
-         items.forEach(function(item) {
-           item.active = true;
-         });
-
-         if (that.checkActivate(that.inputs)) {
-            that.emit('active', that.name);
-         }
-      }
-
-      that.inputs[index].source.on('deactivate', function (sourceName) {
-         sourceInactive(sourceName);
+         that.sourceIsActive(sourceName);
       });
 
       that.inputs[index].source.on('deactivate', function (sourceName) {
-         sourceInactive(sourceName);
+         that.sourceInactive(sourceName);
       });
 
-      var sourceInactive = function(sourceName) {
-         console.log(that.name + ' : Input source ' + sourceName + ' inactive!');
-         
-         // find the input in my array
-         items = that.inputs.filter(function(item) {
-           return (item.source.name == sourceName);
-         });
-
-         // set source input to inactive
-         items.forEach(function(item) {
-           item.active = false;
-         });
-
-         if (that.checkDeactivate(that.inputs)) {
-            that.emit('inactive', that.name);
-         }
-      }
-
-      index++;
+      that.inputs[index].source.on('deactivate', function (sourceName) {
+         that.sourceInactive(sourceName);
+      });
    });
 }
 
 util.inherits(LogicActivator, Activator);
+
+LogicActivator.prototype.sourceIsActive = function(sourceName) {
+   console.log(this.name + ': Input source ' + sourceName + ' active!');
+         
+   // find the input in my array
+   items = this.inputs.filter(function(item) {
+      return (item.source.name == sourceName);
+   });
+
+   // set source input to active
+   items.forEach(function(item) {
+      item.active = true;
+   });
+
+   if (this.checkActivate(that.inputs)) {
+      this.emit('active', that.name);
+   }
+}
+
+LogicActivator.prototype.sourceIsInActive = function(sourceName) {
+   console.log(that.name + ' : Input source ' + sourceName + ' inactive!');
+         
+   // find the input in my array
+   items = this.inputs.filter(function(item) {
+      return (item.source.name == sourceName);
+   });
+
+   // set source input to inactive
+   items.forEach(function(item) {
+      item.active = false;
+   });
+
+   if (this.checkDeactivate(that.inputs)) {
+      this.emit('inactive', that.name);
+   }
+}
 
 module.exports = exports = LogicActivator;
