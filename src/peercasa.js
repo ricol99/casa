72ab5cee248f70bd725d78850ae1c31429efbe87
Richard Collin <richard@collin.me.uk>
2015-03-12 19:32:39 +0000

    bugs

--- src/peercasa.js
+++ src/peercasa.js
@@ -76,7 +76,7 @@ function PeerCasa(_config) {
                   that.connected = false;
                   clearInterval(that.intervalID);
                   that.intervalID = null;
-                  that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name });
+                  that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name }});
                   that.socket = null;
                   that.emit('inactive', { sourceName: that.name });
                }
@@ -238,7 +238,7 @@ PeerCasa.prototype.connectToPeerCasa = function() {
          that.connected = false;
          clearInterval(that.intervalID);
          that.intervalID = null;
-         that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name });
+         that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name }});
          that.invalidateSources();
          that.emit('inactive', { sourceName: that.name });
       }
@@ -252,7 +252,7 @@ PeerCasa.prototype.connectToPeerCasa = function() {
          that.connected = false;
          clearInterval(that.intervalID);
          that.intervalID = null;
-         that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name });
+         that.emit('broadcast-message', { message: 'casa-inactive', data: { sourceName: that.name, sourceCasa: that.name }});
          that.invalidateSources();
          that.emit('inactive', { sourceName: that.name });
       }
