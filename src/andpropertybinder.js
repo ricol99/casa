7f7c0ab310ef4bfec8e587e8d626237797479ed1
Richard Collin <Richard.Collin@thomsonreuters.com>
2015-08-28 11:53:22 +0100

    Adding new propertybinders for mulitple inputs with logic for AND, OR and XOR

---
+++ src/andpropertybinder.js
@@ -0,0 +1,26 @@
+var util = require('util');
+var SourceMergePropertyBinder = require('./sourcemergepropertybinder');
+
+function AndPropertyBinder(_config, _owner) {
+
+   _config.allInputsRequiredForValidity = true;
+   SourceMergePropertyBinder.call(this, _config, _owner);
+
+   var that = this;
+}
+
+util.inherits(AndPropertyBinder, SourceMergePropertyBinder);
+
+AndActivator.prototype.checkActivate = function() {
+   // all inputs active
+   for (var prop in this.multiSourceListener.sourceAttributes) {
+
+      if (this.multiSourceListener.sourceAttributes.hasOwnProperty(prop) && this.multiSourceListener.sourceAttributes[prop] && !this.multiSourceListener.sourceAttributes[prop].active) {
+         return false;
+      }
+   }
+
+   return true;
+};
+
+module.exports = exports = AndPropertyBinder;
---
+++ src/orpropertybinder.js
@@ -0,0 +1,26 @@
+var util = require('util');
+var SourceMergePropertyBinder = require('./sourcemergepropertybinder');
+
+function OrPropertyBinder(_config, _owner) {
+
+   _config.allInputsRequiredForValidity = false;
+   SourceMergePropertyBinder.call(this, _config, _owner);
+
+   var that = this;
+}
+
+util.inherits(OrPropertyBinder, SourceMergePropertyBinder);
+
+OrPropertyBinder.prototype.checkActivate = function() {
+   // any input active
+   for (var prop in this.multiSourceListener.sourceAttributes) {
+
+      if (this.multiSourceListener.sourceAttributes.hasOwnProperty(prop) && this.multiSourceListener.sourceAttributes[prop] && this.multiSourceListener.sourceAttributes[prop].active) {
+         return true;
+      }
+   }
+
+   return false;
+};
+
+module.exports = exports = OrPropertyBinder;
