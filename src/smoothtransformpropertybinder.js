7247a6ff200a06bc5352816d01be246c9611a021
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-01-20 14:41:04 +0000

    bugs

--- src/smoothtransformpropertybinder.js
+++ src/smoothtransformpropertybinder.js
@@ -4,11 +4,11 @@ var PropertyBinder = require('./propertybinder');
 function SmoothTransformPropertyBinder(_config, _owner) {
 
    this.rate = _config.rate;                    // Change allowed per second
-   this.resolution = _config.resolution;        // Refresh rate in seconds
+   this.resolution = (_config.resolution == undefined) ? 1 : _config.resolution;
 
    this.floorOutput = (_config.floorOutput == undefined) ? function(_input) { return Math.floor(_input); } : function(_input) { return _input; };
-   this.calcuatedResolution = _config.resolution;
-   this.targetOutput = 0;
+   this.calculatedResolution = _config.resolution;
+   this.targetValue = 0;
    this.timeoutObj = null;
 
    PropertyBinder.call(this, _config, _owner);
@@ -32,17 +32,17 @@ function copyData(_sourceData) {
 function restartTimer(_that) {
 
    if (_that.timeoutObj) {
-      _that.clearTimeout(_that.timeoutObj);
+      clearTimeout(_that.timeoutObj);
    }
 
    _that.timeoutObj = setTimeout(function(_this) {
       _this.timeoutObj = null;
 
       if (_this.binderEnabled) {
-        var difference = _this.targetOutput - _this.myPropertyValue();
+        var difference = _this.targetValue - _this.myPropertyValue();
 
-         if (Math.abs(_this.step) < Math.abs(difference)) {
-            _this.updatePropertyAfterRead(_this.targetOutput, _this.lastData);
+         if (Math.abs(difference) <= Math.abs(_this.step)) {
+            _this.updatePropertyAfterRead(_this.targetValue, _this.lastData);
          }
          else {
             _this.updatePropertyAfterRead(_this.floorOutput(_this.myPropertyValue() + _this.step), _this.lastData);
@@ -56,20 +56,17 @@ SmoothTransformPropertyBinder.prototype.newPropertyValueReceivedFromSource = fun
    this.lastData = copyData(_data);
    var propValue = _data.propertyValue;
 
-   if (this.targetOutput != propValue) {
-      this.targetOutput = propValue;
+   if (this.targetValue != propValue) {
+      this.targetValue = propValue;
 
-      var difference = this.targetOutput - this.myPropertyValue;
+      var difference = this.targetValue - this.myPropertyValue();
 
-      if (this.resolution == undefined) {
-         var totalTimeToChange = Math.abs(difference) / rate;
-         var timeToChangeByOne =  totalTimeToChange / Math.abs(difference);
-         this.calculatedResolution = (this.resolution != undefined) ? timeToChangeByOne : this.resolution;
-      }
+      var totalTimeToChange = Math.abs(difference) / this.rate;
+      var timeToChangeByOne =  totalTimeToChange / Math.abs(difference);
+      this.calculatedResolution = timeToChangeByOne * this.resolution;
+      this.step = ((difference > 0) ? 1 : -1) * this.resolution;
 
-      this.step = difference / this.calculatedResolution;
-
-      if (Math.abs(this.step) < Math.abs(difference)) {
+      if (Math.abs(this.targetValue - this.myPropertyValue()) <= Math.abs(this.step)) {
          this.updatePropertyAfterRead(propValue, _data);
       }
       else {
