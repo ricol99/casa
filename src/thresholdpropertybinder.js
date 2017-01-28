4ba0a024d977bdacc01f1ada04c5953b2cf20204
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-01-28 16:22:53 +0000

    bug

--- src/thresholdpropertybinder.js
+++ src/thresholdpropertybinder.js
@@ -7,6 +7,8 @@ function ThresholdPropertyBinder(_config, _owner) {
    this.thresholds = (_config.threshold != undefined) ? [_config.threshold] : _config.thresholds;
    this.buffer = _config.buffer;
    this.activeThreshold = -1;
+   this.lastValue = 0;
+   this.cold = true;
 
    PropertyBinder.call(this, _config, _owner);
 }
@@ -16,30 +18,36 @@ util.inherits(ThresholdPropertyBinder, PropertyBinder);
 ThresholdPropertyBinder.prototype.newPropertyValueReceivedFromSource = function(_sourceListener, _data) {
    var newPropertyValue = _data.propertyValue;
 
-   if (this.activeThreshold == -1) {
+   if (this.cold) {
+      this.cold = false;
+   }
+   else {
+      if (this.activeThreshold == -1) {
 
-      for (var index = 0; index < this.thresholds.length; ++index) {
+         for (var index = 0; index < this.thresholds.length; ++index) {
 
-         if (((this.myPropertyValue >= this.thresholds[index]) && (_data.propertyValue <= this.thresholds[index])) ||
-             ((this.myPropertyValue <= this.thresholds[index]) && (_data.propertyValue >= this.thresholds[index]))) {
+            if (((this.lastValue >= this.thresholds[index]) && (_data.propertyValue <= this.thresholds[index])) ||
+                ((this.lastValue <= this.thresholds[index]) && (_data.propertyValue >= this.thresholds[index]))) {
 
-            this.activeThreshold = index;
-            break;
+               this.activeThreshold = index;
+               break;
+            }
+         }
+      }
+
+      if (this.activeThreshold != -1) {
+         // We are currently buffering
+
+         if (Math.abs(_data.propertyValue - this.thresholds[this.activeThreshold]) > this.buffer) {
+            this.activeThreshold = -1;
+         }
+         else {
+            newPropertyValue = this.thresholds[this.activeThreshold];
          }
       }
    }
 
-   if (this.activeThreshold != -1) {
-      // We are currently buffering
-
-      if (Math.abs(_data.propertyValue - this.thresholds[this.activeThreshold]) > this.buffer) {
-         this.activeThreshold = -1;
-      }
-      else {
-         newPropertyValue = this.thresholds[this.activeThreshold];
-      }
-   }
-
+   this.lastValue = newPropertyValue;
    this.updatePropertyAfterRead(newPropertyValue, _data);
 };
 
