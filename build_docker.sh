051cdb6faec608c0db14b3c6c635bc77decea2b7
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-02-17 14:54:55 +0000

    Moving config of production network to secure mode

--- build_docker.sh
+++ build_docker.sh
@@ -7,7 +7,7 @@ hyper pull ricol99/casa
 hyper stop `hyper ps -a | tail -1 | awk '{print $1}'`
 hyper rm `hyper ps -a | tail -1 | awk '{print $1}'`
 hyper rmi `hyper images | tail -1 | awk '{print $3}'`
-CONTAINERID=`hyper create --size=s2 --name casa -p 80:8096 ricol99/casa | awk '{print $1}'`
+CONTAINERID=`hyper create --size=s2 --name casa -p 443:8096 ricol99/casa | awk '{print $1}'`
 hyper fip attach 199.245.56.152 $CONTAINERID
 hyper start $CONTAINERID
 sleep 2
