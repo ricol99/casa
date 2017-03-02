ed9185a28d89a2e0a1e89130423d956aa74037f5
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-03-02 23:11:51 +0000

    sonos property monitoring and alarm support

--- src/sonosplayer.js
+++ src/sonosplayer.js
@@ -10,10 +10,32 @@ function SonosPlayer(_config) {
    this.casaSys = CasaSystem.mainInstance();
    this.zone = _config.zone;
    this.host = _config.host;
+   this.alarmUrls = {};
+   this.alarmRepeatTimes = {};
+   this.alarmVolumes = {};
+   this.notifications = {};
+   this.inAlarmStatus = false;
 
    this.props['volume']  = new Property({ name: 'volume', type: 'property', initialValue: 0 }, this);
+   this.props['volume-writable']  = new Property({ name: 'volume-writable', type: 'property', initialValue: false }, this);
+   this.props['muted']  = new Property({ name: 'muted', type: 'property', initialValue: false }, this);
    this.props['playing']  = new Property({ name: 'playing', type: 'property', initialValue: false }, this);
    this.props['current-track']  = new Property({ name: 'current-track', type: 'property', initialValue: "" }, this);
+   this.props['play-mode']  = new Property({ name: 'play-mode', type: 'property', initialValue: "" }, this);
+
+   if (_config.alarmUrl) {
+      this.alarmUrls['alarm'] = _config.alarmUrl;
+      this.alarmRepeatTimes['alarm'] = _config.alarmRepeatTime;
+      this.alarmVolumes['alarm'] = _config.hasOwnProperty("alarmVolume") ? _config.alarmVolume : 60;
+      this.props['alarm']  = new Property({ name: 'alarm', type: 'property', initialValue: false }, this);
+   }
+
+   if (_config.fireAlarmUrl) {
+      this.alarmUrls['fire-alarm'] = _config.fireAlarmUrl;
+      this.alarmRepeatTimes['fire-alarm'] = _config.fireAlarmRepeatTime;
+      this.alarmVolumes['fire-alarm'] = _config.hasOwnProperty("fireAlarmVolume") ? _config.fireAlarmVolume : 60;
+      this.props['fire-alarm']  = new Property({ name: 'fire-alarm', type: 'property', initialValue: false }, this);
+   }
 }
 
 util.inherits(SonosPlayer, Thing);
@@ -30,20 +52,82 @@ SonosPlayer.prototype.coldStart = function() {
 
       if (!this.sonosService) {
          console.error(this.uName + ": ***** Sonos service not found! *************");
-         process.exit();
+         process.exit(1);
       }
 
-      this.sonosService.registerForHostForZone(this.zone, function(_err, _host) {
+      this.sonosService.registerForHostForZone(this.zone, function(_err, _device) {
 
          if (!_err) {
-            that.props['ACTIVE'].setProperty(true, { sourceName: that.uName });
-            that.host = _host;
-            that.sonos = new Sonos.Sonos(that.host);
+            that.establishPlayerConnection(_device);
          }
       });
    }
 
    Thing.prototype.coldStart.call(this);
+};
+
+SonosPlayer.prototype.establishPlayerConnection = function(_device) {
+   var that = this;
+
+   this.props['ACTIVE'].setProperty(true, { sourceName: this.uName });
+   this.device = _device;
+   this.sonos = new Sonos.Sonos(this.device.host, this.device.port);
+   this.localListeningPort = this.sonosService.grabLocalListeningPort();
+
+   // XX TODO Remove hack
+   var SonosListener = require('./node_modules/sonos/lib/events/listener');
+   this.sonosListener = new SonosListener(this.device, { port: this.localListeningPort });
+   this.sonosListener.listen(function(_err, _result) {
+
+      if (_err) {
+         console.error(that.uName + ": ***** Unable to start Sonos listener: " + _err);
+         process.exit(1);
+      }
+
+      that.startSyncingStatus();
+   });
+};
+
+function internalListener(_owner, _endpoint, _handler) {
+
+   _owner.sonosListener.addService(_endpoint, function(_err, _sid) {
+
+      if (_err) {
+         console.error(_owner.uName + ": Unable to sync device status! Error: ", _err);
+      }
+      else {
+         _owner.notifications[_sid] = _handler;
+      }
+   });
+};
+
+SonosPlayer.prototype.startSyncingStatus = function() {
+   var that = this;
+
+   var renderingControlListener =  new internalListener(this, '/MediaRenderer/GroupRenderingControl/Event', SonosPlayer.prototype.processRenderControlChange);
+   var avTransportControlListener = new internalListener(this, '/MediaRenderer/AVTransport/Control', SonosPlayer.prototype.processAVTransportControlChange);
+
+   this.sonosListener.on('serviceEvent', function(_endpoint, _sid, _data) {
+      console.log(that.uName + ": Received notifcation from device. Endpoint="+_endpoint+" Sid="+_sid+" Data=",_data);
+
+      if (that.notifications[_sid]) {
+         that.notifications[_sid].call(that, _data);
+      }
+      else {
+         console.error(that.uName + ": Received notification for sid that has no handler. Sid=", _sid);
+      }
+   });
+};
+
+SonosPlayer.prototype.processRenderControlChange = function(_data) {
+   console.log(this.uName + ": processRenderControlChange()");
+   this.props['volume'].setProperty(_data.GroupVolume, { sourceName: this.uName });
+   this.props['muted'].setProperty((_data.GroupMute != 0), { sourceName: this.uName });
+   this.props['volume-writable'].setProperty((_data.GroupVolumeChangeable == 0), { sourceName: this.uName });
+};
+
+SonosPlayer.prototype.processAVTransportControlChange = function(_data) {
+   console.log(this.uName + ": processAVTransportControlChange()");
 };
 
 SonosPlayer.prototype.propertyAboutToChange = function(_propName, _propValue, _data) {
@@ -53,21 +137,24 @@ SonosPlayer.prototype.propertyAboutToChange = function(_propName, _propValue, _d
 
       switch (_propName) {
       case "volume": 
-         setVolume(this, _propValue);
+         if (this.props['volume-writable'].value) this.setVolume(_propValue);
+         break;
+
+      case "muted": 
+         this.setMuted(_propValue);
          break;
 
       case "playing":
-
-         if (_propValue) {
-            play(_this);
-         }
-         else {
-            pause(_this);
-         }
+         (_propValue) ? this.play() : this.pause();
          break;
 
       case "current-track":
-         play(this, _propValue);
+         this.play(_propValue);
+         break;
+
+      case "fire-alarm":
+      case "alarm":
+         this.alarm(_propName, _propValue);
          break;
 
       default:
@@ -75,10 +162,10 @@ SonosPlayer.prototype.propertyAboutToChange = function(_propName, _propValue, _d
    }
 };
 
-function setVolume(_this, _level) {
-   var that = _this;
+SonosPlayer.prototype.setVolume = function(_level) {
+   var that = this;
 
-   _this.sonos.setVolume(_level, function(_err, _result) {
+   this.sonos.setVolume(_level, function(_err, _result) {
 
       if (_err) {
          console.log(that.uName + ": Unable to set Volume!");
@@ -86,10 +173,21 @@ function setVolume(_this, _level) {
    });
 };
 
-function pause(_this, _level) {
-   var that = _this;
+SonosPlayer.prototype.setMuted = function(_muted) {
+   var that = this;
 
-   _this.sonos.pause(_level, function(_err, _result) {
+   this.sonos.setMuted(_muted, function(_err, _result) {
+
+      if (_err) {
+         console.log(that.uName + ": Unable to set mute!");
+      }
+   });
+};
+
+SonosPlayer.prototype.pause = function(_level) {
+   var that = this;
+
+   this.sonos.pause(_level, function(_err, _result) {
 
       if (_err) {
          console.log(that.uName + ": Unable to pause!");
@@ -97,11 +195,11 @@ function pause(_this, _level) {
    });
 };
 
-function play(_this, _url) {
-   var that = _this;
+SonosPlayer.prototype.play = function(_url) {
+   var that = this;
 
    if (url) {
-      _this.sonos.queueNext(_url, function(_err, _playing) {
+      this.sonos.queueNext(_url, function(_err, _playing) {
 
          if (_err) {
             console.log(that.uName + ": Unable to queue next track!");
@@ -124,6 +222,125 @@ function play(_this, _url) {
          }
       });
    }
+};
+
+SonosPlayer.prototype.playNext = function(_stopIfFail) {
+   var that = this;
+
+   this.sonos.next(function(_err, _result) {
+
+      if (_err) {
+         console.log(that.uName + ": Unable to play next track!");
+      }
+
+      if (!_result && _stopIfFail) {
+         that.stop();
+      }
+   });
+};
+
+SonosPlayer.prototype.stop = function() {
+   var that = this;
+
+   this.sonos.stop(function(_err, _result) {
+      console.log(that.uName + ": Unable to stop music from playing!");
+   });
+};
+
+SonosPlayer.prototype.saveCurrentState = function(_property) {
+   this.savedStatus = { volume: this.props['volume'].value,
+                        muted: this.props['muted'].value,
+                        playing: this.props['playing'].value };
+
+   if (this.props['muted'].value) {
+      this.props['muted'].setProperty(false);
+   }
+
+   this.props['volume'].setProperty(this.alarmVolumes[_property]);
+};
+
+SonosPlayer.prototype.restoreSavedState = function() {
+
+   if (this.props['volume'].value !== this.savedStatus.volume) {
+      this.props['volume'].setProperty(this.savedStatus.volume);
+   }
+
+   if (this.props['muted'].value !== this.savedStatus.muted) {
+      this.props['muted'].setProperty(this.savedStatus.muted);
+   }
+
+   if (this.savedStatus.playing) {
+      this.playNext(true);
+   }
+   else {
+      this.stop();
+   }
+};
+
+SonosPlayer.prototype.alarm = function(_propName, _propValue) {
+
+   if (this.inAlarmStatus === _propValue) {
+      return;
+   }
+
+   this.inAlarmStatus = _propValue;
+
+   if (_propValue) {
+      this.saveCurrentState(_propName);
+      this.play(this.alarmUrls[_propName]);
+      this.replayAfterTimeout(this.alarmUrls[_propName], this.alarmRepeatTimes[_propName]);
+   }
+   else {
+      clearTimeout(this.repeatTimer);
+      this.restoreSavedState();
+   }
+};
+
+SonosPlayer.prototype.replayAfterTimeout = function(_url, _timeout) {
+
+   this.repeatTimer = setTimeout(function(_this) {
+
+      if (_this.inAlarmStatus) {
+
+         this.sonos.previous(function(_err, _result) {
+
+            if (err) {
+               console.log(this.uName + ": Unable to call previous track");
+            }
+         });
+
+         _this.replayAfterTimeout(_url, _timeout);
+      }
+   }, _timeout, this);
+};
+
+SonosPlayer.prototype.getPlayMode = function(_callback) {
+   var endpoint = '/MediaRenderer/AVTransport/Control';
+   var action = '"urn:schemas-upnp-org:service:AVTransport:1#GetTransportSettings"'
+   var body = '<u:GetTransportSettings xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID></u:GetTransportSettings>'
+   var state = null;
+   var responseTag = 'u:GetTransportSettingsResponse'
+   return this.sonos.request(endpoint, action, body, responseTag, function (_err, _data) {
+
+      if (_err) {
+         _callback(_err);
+         return;
+      }
+
+      if (JSON.stringify(_data[0].PlayMode) === '["NORMAL"]') {
+         state = 'normal';
+      } else if (JSON.stringify(_data[0].PlayMode) === '["REPEAT_ALL"]') {
+         state = 'repeat_all';
+      } else if (JSON.stringify(_data[0].PlayMode) === '["SHUFFLE"]') {
+         state = 'shuffle';
+      } else if (JSON.stringify(_data[0].PlayMode) === '["SHUFFLE_NOREPEAT"]') {
+         state = 'shuffle_norepeat';
+      } else if (JSON.stringify(_data[0].PlayMode) === '["NO_MEDIA_PRESENT"]') {
+         state = 'no_media';
+      }
+
+      return _callback(_err, state);
+  });
 };
 
 module.exports = exports = SonosPlayer;
--- src/sonosservice.js
+++ src/sonosservice.js
@@ -7,6 +7,8 @@ function SonosService(_config) {
    Service.call(this, _config);
    this.players = {};
    this.callbacks = {};
+   this.portStart = _config.hasOwnProperty("portStart") ? _config.portStart : 12000;
+   this.currentPort = this.portStart;
 }
 
 util.inherits(SonosService, Service);
@@ -21,7 +23,7 @@ SonosService.prototype.registerForHostForZone = function(_zone, _callback) {
    }
 
    if (this.players[_zone]) {
-      _callback(null, this.players[_zone].devices[0].host);
+      _callback(null, this.players[_zone].devices[0]);
    }
 };
 
@@ -33,22 +35,27 @@ SonosService.prototype.coldStart = function() {
      _device.getZoneAttrs(function(_err, _attrs) {
 
         if (!that.players[_attrs.CurrentZoneName]) {
-           that.players[_attrs.CurrentZoneName] = { zone: _attrs.CurrentZoneName, devices: [{ host: _device.host, model: _model }] };
+           that.players[_attrs.CurrentZoneName] = { zone: _attrs.CurrentZoneName, devices: [ _device ] };
            console.log(that.uName + ': Found new Sonos Player for zone '+_attrs.CurrentZoneName+' at ' + _device.host + ', model:' + _model);
 
            if (that.callbacks[_attrs.CurrentZoneName]) {
 
               for (var i = 0; i < that.callbacks[_attrs.CurrentZoneName].length; ++i) {
-                 that.callbacks[_attrs.CurrentZoneName][i](null, that.players[_attrs.CurrentZoneName].devices[0].host);
+                 that.callbacks[_attrs.CurrentZoneName][i](null, that.players[_attrs.CurrentZoneName].devices[0]);
               }
            }
         }
         else {
-           that.players[_attrs.CurrentZoneName].devices.push({ host: _device.host, model: _model });
+           that.players[_attrs.CurrentZoneName].devices.push(_device);
            console.log(that.uName + ': Adding host '+_device.host + ' to zone '+ _attrs.CurrentZoneName + ', model: ' + _model);
         }
      });
    });
+};
+
+SonosService.prototype.grabLocalListeningPort = function() {
+   this.currentPort++;
+   return this.currentPort - 1;
 };
 
 module.exports = exports = SonosService;
