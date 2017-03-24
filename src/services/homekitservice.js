77aeebf82ea96c3def8ed09f47aa2eda0c954c28
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-03-24 10:54:44 +0000

    Homekit bridge capability

---
+++ src/services/homekitservice.js
@@ -0,0 +1,59 @@
+var util = require('util');
+var Service = require('../service');
+var storage = require('node-persist');
+const HAP = require('hap-nodejs');
+HAP.init();
+
+var Bridge = HAP.Bridge;
+var Accessory = HAP.Accessory;
+var Service = HAP.Service;
+var Characteristic = HAP.Characteristic;
+var uuid = HAP.uuid;
+
+storage.initSync();
+
+function HomekitService(_config) {
+   Service.call(this, _config);
+   this.port = this.casa.allocatePort(this.uName);
+
+   this.displayName = _config.displayName;
+   this.pincode = _config.pincode;
+   this.username = _config.username;
+   this.manufacturer = (_config.manufacturer == undefined) ? "Casa" : _config.manufacturer;
+   this.model = (_config.model == undefined) ? "v1.0" : _config.model;
+   this.serialNumber = (_config.serialNumber == undefined) ? "XXXXXXX" : _config.serialNumber;
+
+   this.hkUUID = uuid.generate('hap-nodejs:accessories:' + this.thingType + ':' + this.uName);
+   this.hkAccessory = new Accessory(this.displayName, this.hkUUID);
+   this.hkAccessory.username = this.username;
+   this.hkAccessory.pincode = this.pincode;
+}
+
+util.inherits(HomekitService, Service);
+
+HomekitService.prototype.coldStart = function() {
+   var that = this;
+
+   // Start by creating our Bridge which will host all loaded Accessories
+   this.bridge = new Bridge('Casa Homekit Bridge', this.hkUUID));
+
+   // Listen for bridge identification event
+   this.bridge.on('identify', function(_paired, _callback) {
+     console.log("Node Bridge identify");
+     _callback(); // success
+   });
+
+   // Publish the Bridge on the local network.
+   this.bridge.publish({
+     username: that.username,
+     port: that.port,
+     pincode: that.pincode,
+     category: Accessory.Categories.BRIDGE
+   });
+};
+
+HomekitService.prototype.addAccessory = function(_accessory) {
+  this.bridge.addBridgedAccessory(_accessory);
+};
+
+module.exports = exports = HomekitService;
