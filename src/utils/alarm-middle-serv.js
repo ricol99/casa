1d163bd0aa05249601b49b683c728d85ff98fce8
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-03-30 21:48:24 +0100

    bug

--- src/utils/alarm-middle-serv.js
+++ src/utils/alarm-middle-serv.js
@@ -6,6 +6,7 @@ net = require('net');
 function Alarm(_config) {
    this.ipAddress = _config.ipAddress;
    this.port = _config.port;
+   this.connected = false;
 
    events.EventEmitter.call(this);
 }
@@ -20,10 +21,19 @@ Alarm.prototype.connect = function() {
 
    this.socket.on('connect', function(_buffer) {
       console.log('Connected to alarm');
+      that.connected = true;
+
+      if (that.storedBuffer) {
+         setTimeout(function(_this) {
+            //console.log("Middle->Alarm: "+_this.storedBuffer.toString('ascii')+ " :: ", _this.storedBuffer);
+            console.log("Middle->Alarm: ", _this.storedBuffer);
+            _this.socket.write(_this.storedBuffer);
+         }, 1500, that);
+      }
    });
 
    this.socket.on('data', function(_buffer) {
-      console.log("Alarm->Middle: "+_buffer.toString('ascii')+ " :: ", _buffer);
+      console.log("Alarm->Middle: ", _buffer);
       that.emit('data', _buffer);
    });
 
@@ -35,15 +45,22 @@ Alarm.prototype.connect = function() {
 };
 
 Alarm.prototype.write = function(_buffer, _encoding, _callback) {
-   console.log("Middle->Alarm: "+_buffer.toString('ascii')+ " :: ", _buffer);
-   this.socket.write(_buffer, _encoding, _callback);
+
+   if (!this.connected) {
+      this.storedBuffer = _buffer;
+   }
+   else {
+      console.log("Middle->Alarm: ", _buffer);
+      this.socket.write(_buffer, _encoding, _callback);
+   }
+};
+
+Alarm.prototype.end = function() {
+   this.socket.end();
 };
 
 // Keep track of the chat clients
 var clients = [];
-
-var alarm = new Alarm({ ipAddress: "192.168.1.85", port: 10001 });
-alarm.connect();
 
 // Start a TCP Server
 net.createServer(function (socket) {
@@ -51,22 +68,21 @@ net.createServer(function (socket) {
   // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
 
+  var alarm = new Alarm({ ipAddress: "192.168.1.85", port: 10001 });
+  alarm.connect();
+
   alarm.on('data', function(_buffer) {
-     console.log("Middle->Client: "+_buffer.toString('ascii')+ " :: ", _buffer);
+     console.log("Middle->Client: ", _buffer);
      socket.write(_buffer);
   });
 
   // Put this new client in the list
   clients.push(socket);
-
-  // Send a nice welcome message and announce
-  //socket.write("Welcome " + socket.name + "\n");
-  //broadcast(socket.name + " joined the chat\n", socket);
   console.log("New client: " + socket.name);
 
   // Handle incoming messages from clients.
   socket.on('data', function (_buffer) {
-    console.log("Client->Middle: "+_buffer.toString('ascii')+" :: ", _buffer);
+    console.log("Client->Middle: ", _buffer);
     alarm.write(_buffer);
   });
 
@@ -74,6 +90,7 @@ net.createServer(function (socket) {
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
     console.log("Client disconnected");
+    alarm.end();
   });
 
 }).listen(10001);
