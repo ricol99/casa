619166894f390a57f5ad08ef50f07ae9adacf91d
Richard Collin <richard@collin.me.uk>
2017-05-26 20:11:03 +0100

    bug

--- src/thing.js
+++ src/thing.js
@@ -31,11 +31,14 @@ Thing.prototype.updateProperty = function(_propName, _propValue, _data) {
 
    if (data.alignWithParent) {
 
-      if (data.enterManualMode) {
-         this.props[_propName].setManualMode(true); // XXXX TODO Is this the right thing to do - who controls the timers?
-      }
-      else if (data.leaveManualMode) {
-         this.props[_propName].setManualMode(false); // XXXX TODO Is this the right thing to do - who controls the timers?
+      if (this.props[_propName]) {
+
+         if (data.enterManualMode) {
+            this.props[_propName].setManualMode(true); // XXXX TODO Is this the right thing to do - who controls the timers?
+         }
+         else if (data.leaveManualMode) {
+            this.props[_propName].setManualMode(false); // XXXX TODO Is this the right thing to do - who controls the timers?
+         }
       }
 
       if (!Source.prototype.updateProperty.call(this, _propName, _propValue, data)) {
