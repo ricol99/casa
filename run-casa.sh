940948fd1a7d210cd1999fa5a9e98bf093b247d3
Richard Collin <richard@collin.me.uk>
2017-05-28 13:13:25 +0100

    run script

--- run-casa.sh
+++ run-casa.sh
@@ -6,8 +6,8 @@ cd /home/pi/dev/casa/src
 RC=1
 while [ $RC -ne 0 ]; do
    sudo -u pi git pull origin master
-   cp /home/pi/dev/casa/log.txt /home/pi/dev/casa/last-log.txt 2> /dev/null
-   cp /home/pi/dev/casa/error.txt /home/pi/dev/casa/last-error.txt 2> /dev/null
+   mv /home/pi/dev/casa/log.txt /home/pi/dev/casa/last-log.txt 2> /dev/null
+   mv /home/pi/dev/casa/error.txt /home/pi/dev/casa/last-error.txt 2> /dev/null
    node app.js --system configs/casa-collin-config.json configs/env-config.json --secure >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
    RC=$?
 done
