ac5db0bc56c2531ce38ca3a91c3a3d107c003204
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-08-25 10:02:01 +0100

    small changes to try to fix lost events in lightwaverf service

--- src/services/lightwaverfservice.js
+++ src/services/lightwaverfservice.js
@@ -18,62 +18,52 @@ LightwaveRfService.prototype.coldStart = function() {
 
 LightwaveRfService.prototype.turnDeviceOn = function(_roomId, _deviceId, _callback) {
 
-   this.addToQueue(function(_this, _params, _callback) {
+   this.addToQueue(function(_this, _params, _cb) {
       console.log(_this.uName + ': turning device on, roomId: ' + _params.roomId + ', deviceId: ' + _params.deviceId);
-      _this.lightwaveRf.turnDeviceOn(_params.roomId, _params.deviceId, _callback);
+      _this.lightwaveRf.turnDeviceOn(_params.roomId, _params.deviceId, _cb);
    }, { roomId: _roomId, deviceId: _deviceId } , _callback);
 
-   if (!this.requestPending) {
-      this.makeNextRequest();
-   }
+   this.makeNextRequest();
 }
 
 LightwaveRfService.prototype.turnDeviceOff = function(_roomId, _deviceId, _callback) {
 
-   this.addToQueue(function(_this, _params, _callback) {
+   this.addToQueue(function(_this, _params, _cb) {
       console.log(_this.uName + ': turning device off, roomId: ' + _params.roomId + ', deviceId: ' + _params.deviceId);
-      _this.lightwaveRf.turnDeviceOff(_params.roomId, _params.deviceId, _callback);
+      _this.lightwaveRf.turnDeviceOff(_params.roomId, _params.deviceId, _cb);
    }, { roomId: _roomId, deviceId: _deviceId } , _callback);
 
-   if (!this.requestPending) {
-      this.makeNextRequest();
-   }
+   this.makeNextRequest();
 }
 
 LightwaveRfService.prototype.setDeviceDim = function(_roomId, _deviceId, _dimLevel, _callback) {
 
-   this.addToQueue(function(_this, _params, _callback) {
+   this.addToQueue(function(_this, _params, _cb) {
       console.log(_this.uName + ': turning device on with dim level, roomId: ' + _params.roomId + ', _deviceId: ' + _params.deviceId + ', dimLevel: ' + _params.dimLevel);
-      _this.lightwaveRf.setDeviceDim(_params.roomId, _params.deviceId, _params.dimLevel, _callback);
+      _this.lightwaveRf.setDeviceDim(_params.roomId, _params.deviceId, _params.dimLevel, _cb);
    }, { roomId: _roomId, deviceId: _deviceId, dimLevel: _dimLevel } , _callback);
 
-   if (!this.requestPending) {
-      this.makeNextRequest();
-   }
+   this.makeNextRequest();
 }
 
 LightwaveRfService.prototype.setRoomMood = function(_roomId, _moodId, _callback) {
 
-   this.addToQueue(function(_this, _params, _callback) {
+   this.addToQueue(function(_this, _params, _cb) {
       console.log(_this.uName + ': setting room mood, roomId: ' + _params.roomId + ' moodId:' + _params.moodId);
-      _this.lightwaveRf.setMood(_params.roomId, _params.moodId, _callback);
+      _this.lightwaveRf.setMood(_params.roomId, _params.moodId, _cb);
    }, { roomId: _roomId, moodId: _moodId } , _callback);
 
-   if (!this.requestPending) {
-      this.makeNextRequest();
-   }
+   this.makeNextRequest();
 }
 
 LightwaveRfService.prototype.turnRoomOff = function(_roomId, _callback) {
 
-   this.addToQueue(function(_this, _params, _callback) {
+   this.addToQueue(function(_this, _params, _cb) {
       console.log(_this.uName + ': turning room off, roomId: ' + _params.roomId);
-      _this.lightwaveRf.turnRoomOff(_params.roomId, _callback);
+      _this.lightwaveRf.turnRoomOff(_params.roomId, _cb);
    }, { roomId: _roomId } , _callback);
 
-   if (!this.requestPending) {
-      this.makeNextRequest();
-   }
+   this.makeNextRequest();
 }
 
 LightwaveRfService.prototype.addToQueue = function(_request, _params, _callback) {
@@ -83,7 +73,7 @@ LightwaveRfService.prototype.addToQueue = function(_request, _params, _callback)
 LightwaveRfService.prototype.makeNextRequest = function() {
    var that = this;
 
-   if (this.queue.length > 0) {
+   if ((this.queue.length > 0 && !this.requestPending) {
       this.requestPending = true;
 
       this.queue[0].request(this, this.queue[0].params, function(_error, _content) {
@@ -91,18 +81,15 @@ LightwaveRfService.prototype.makeNextRequest = function() {
          that.queue.shift().callback(_error, _content);
 
          if (that.queue.length > 0) {
-         console.log(that.uName+": AAAAA queue length="+that.queue.length);
 
             // More in the queue, so reschedule after the link has had time to settle down
             var delay = setTimeout(function(_this) {
                _this.requestPending = false;
-               console.log(_this.uName+": AAAAA next request!" );
                _this.makeNextRequest();
             }, 750, that);
          }
          else {
             that.requestPending = false;
-            console.log(that.uName+": AAAAA no requests levft in queue!" );
          }
       });
    }
