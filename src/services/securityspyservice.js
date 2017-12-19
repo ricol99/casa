c2c83b32a6f7989a5336c1d01bfb5fdebe96810d
Richard Collin <Richard.Collin@thomsonreuters.com>
2017-12-19 09:05:05 -0500

    Working securityspy service - yet to test securityspycamera thing

--- src/services/securityspyservice.js
+++ src/services/securityspyservice.js
@@ -16,56 +16,132 @@ function SecuritySpyService(_config) {
    this.options = { hostname: this.hostname, port: this.port, auth: this.userId + ':' + this.password, rejectUnauthorized: false, requestCert: true, agent: false, timeout: this.requestTimeout*1000 };
 
    this.queue = [];
-   this.registeredCameras = {};
+   this.cameras = {};
    this.requestPending = false;
+   this.initialInfoReceived = false;
 }
 
 util.inherits(SecuritySpyService, Service);
 
 SecuritySpyService.prototype.coldStart = function() {
    var that = this;
-   this.getCameraModes(0, function(_err, _result) {
+   this.systemInfo = new SystemInfo(this);
+
+   this.systemInfo.sync(function(_err, _result) {
 
       if (!_err) {
-         console.log(that.uName + ": AAAAAA Result=", _result);
          that.liveStreamListener = new LiveStreamListener(that);
          that.liveStreamListener.start();
       }
    });
 };
 
-SecuritySpyService.prototype.registerCamera = function(_cameraId, _camera) {
-   this.cameras[_cameraId.toString()] = _camera;
+SecuritySpyService.prototype.registerCamera = function(_cameraId, _cameraObj) {
+
+   if (!this.cameras[_cameraId.toString()]) {
+      this.cameras[_cameraId.toString()] = {};
+      this.cameras[_cameraId.toString()].active = false;
+      this.cameras[_cameraId.toString()][continuous-capture] = false;
+      this.cameras[_cameraId.toString()][motion-capture] = false;
+      this.cameras[_cameraId.toString()].actions = false;
+   }
+
+   this.cameras[_cameraId.toString()].cameraObj = _cameraObj;
+
+   if (this.initialInfoReceived) {
+      this.syncWithCameraObj(_cameraId);
+   }
 };
 
 SecuritySpyService.prototype.deregisterCamera = function(_cameraId, _camera) {
-   delete this.cameras[_cameraId.toString()];
+
+   if (this.cameras[_cameraId.toString()]) {
+      this.cameras[_cameraId.toString()].cameraObj = null;
+   }
+};
+
+SecuritySpyService.prototype.needToSync = function(_cameraId, _property, _value) {
+   return this.cameras[_cameraId.toString()][_property] === _value;
+};
+
+SecuritySpyService.prototype.cameraPropUpdateReceivedFromServer = function(_cameraId, _props) {
+
+   if (!this.cameras[_cameraId.toString()]) {
+      this.cameras[_cameraId.toString()] = {};
+   }
+
+   for (index in _props) {
+      this.cameras[_cameraId.toString()][index] = _props[index];
+   }
+
+   this.syncWithCameraObj(_cameraId);
+};
+
+SecuritySpyService.prototype.syncWithCameraObj = function(_cameraId) {
+
+   if (!this.cameras[_cameraId.toString()] || (!this.cameras[_cameraId.toString()].cameraObj)) {
+      return;
+   }
+
+   var cameraObj = this.cameras[_cameraId.toString()].cameraObj;
+   var props = [];
+
+   for (prop in this.cameras[_cameraId.toString()]) {
+
+      if (prop == "cameraObj") {
+         continue;
+      }
+
+      if (this.cameras[_cameraId.toString()][prop] !== cameraObj.getProperty(prop)) {
+         props.push({ property: prop, value: this.cameras[_cameraId.toString()][prop] });
+      }
+   }
+
+   if (props.length > 0) {
+      this.addPropertiesForAlignment(props);
+   }
+};
+
+SecuritySpyService.prototype.newEventFromServer = function(_cameraId, _event) {
+
+   if (this.cameras[_cameraId.toString()] && this.cameras[_cameraId.toString()].cameraObj) {
+      this.cameras[_cameraId.toString()].cameraObj.raiseEvent(_event);
+   }
 };
 
 SecuritySpyService.prototype.setContinuousCapture = function(_cameraId, _state, _callback) {
-   this.addToQueue("/++ssControlContinuousCapture?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "contCapture", _callback);
-}
+
+   if (this.needToSync(_cameraId, "continuous-capture", _state)) {
+      this.addToQueue("/++ssControlContinuousCapture?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "contCapture", _callback);
+   }
+};
 
 SecuritySpyService.prototype.setMotionCapture = function(_cameraId, _state, _callback) {
-   this.addToQueue("/++ssControlMotionCapture?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "motionCapture", _callback);
-}
+
+   if (this.needToSync(_cameraId, "motion-capture", _state)) {
+      this.addToQueue("/++ssControlMotionCapture?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "motionCapture", _callback);
+   }
+};
 
 SecuritySpyService.prototype.setActions = function(_cameraId, _state, _callback) {
-   this.addToQueue("/++ssControlActions?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "actions", _callback);
-}
+
+   if (this.needToSync(_cameraId, "actions", _state)) {
+      this.addToQueue("/++ssControlActions?cameraNum=" + cameraId + "&arm=" + (_state ? "1" : "0"), "actions", _callback);
+   }
+};
 
 SecuritySpyService.prototype.triggerMotionRecording = function(_cameraId, _callback) {
    this.addToQueue("/++triggermd?cameraNum=" + cameraId, "triggerMotion", _callback);
-}
+};
 
 SecuritySpyService.prototype.getCameraModes = function(_cameraId, _callback) {
    this.addToQueue("/++cameramodes?cameraNum=" + _cameraId, "cameraModes", _callback);
-}
+};
 
 SecuritySpyService.prototype.addToQueue = function(_path, _requestType, _callback) {
    this.queue.push(new Request(this, _requestType, _path, _callback));
    this.makeNextRequest();
-}
+};
 
 SecuritySpyService.prototype.makeNextRequest = function() {
 
@@ -73,7 +149,7 @@ SecuritySpyService.prototype.makeNextRequest = function() {
       this.requestPending = true;
       this.queue[0].send();
    }
-}
+};
 
 SecuritySpyService.prototype.completeRequest = function(_error, _content) {
    console.log(this.uName + ": Request done!");
@@ -91,7 +167,7 @@ SecuritySpyService.prototype.completeRequest = function(_error, _content) {
    else {
       this.requestPending = false;
    }
-}
+};
 
 function Request(_owner, _requestType, _path, _callback) {
    this.owner = _owner;
@@ -241,8 +317,88 @@ LiveStreamListener.prototype.processLine = function(_line) {
                   ONLINE: { property: "ACTIVE", value: true },
                   OFFLINE: { property: "ACTIVE", value: false } };
 
+   if (!change[changeParam]) {
+      return;
+   }
+
    console.log(this.owner.uName + ": AAAAAA Camera ID=" + cameraId + ", change=", change[changeParam]);
 
+   if (change[changeParam].hasOwnProperty("property")) {
+      var props = {};
+      props[change[changeParam].property] = change[changeParam].value;
+      console.log(this.owner.uName + ": AAAAAA Camera Props=" + JSON.stringify(props));
+      this.owner.cameraPropUpdateReceivedFromServer(cameraId, props);
+   }
+   else {
+      this.owner.newEventFromServer(cameraId, change[changeParam].event);
+   }
+};
+
+function SystemInfo(_owner) {
+   this.http = (_owner.secure) ? require('https') : require('http');
+   this.owner = _owner;
+   this.options = { hostname: _owner.hostname, port: _owner.port, auth: _owner.userId + ':' + _owner.password,
+                    path: "/++systemInfo/", rejectUnauthorized: false, requestCert: true, agent: false, timeout: _owner.requestTimeout*1000 };
+
+   this.data = "";
+}
+
+SystemInfo.prototype.sync = function(_callback) {
+   var that = this;
+
+   this.http.get(this.options, function(_res) {
+      console.log('AAAAA STATUS: ' + _res.statusCode);
+      console.log('AAAAA HEADERS: ' + JSON.stringify(_res.headers));
+
+      if (_res.statusCode != 200) {
+         _res.resume();
+         _callback(_res.StatusCode);
+         return;
+      }
+
+      _res.setEncoding('utf8');
+
+      _res.on('data', function(_data) {
+         that.data = that.data + _data;
+      });
+
+      _res.on('end', function() {
+         that.processAllData(_callback);
+      });
+   }).on('error', function(_err) {
+      console.error(that.owner.uName + ": AAAAA Listener connection error=" + _err.message + ". Will reconnect soon");
+      that.lineOpen = false;
+      that.restartLink();
+   });
+};
+
+SystemInfo.prototype.processAllData = function(_callback) {
+   var that = this;
+   var parseString = require('xml2js').parseString;
+
+   parseString(this.data, function (_err, _result) {
+
+      if (_err) {
+         console.error(that.owner.uName + ": AAAAAA Unable to parse received XML string!");
+         _callback(_err);
+         return;
+      }
+
+      for (index in _result.system.cameralist[0].camera) {
+
+         var props = { "active": _result.system.cameralist[0].camera[index].connected == "yes",
+                       "continuous-capture": _result.system.cameralist[0].camera[index]["mode-c"] == "armed",
+                       "motion-capture": _result.system.cameralist[0].camera[index]["mode-m"] == "armed",
+                       "actions": _result.system.cameralist[0].camera[index]["mode-a"] == "armed" };
+
+         console.log(that.owner.uName + ": AAAAAA Camera Props=" + JSON.stringify(props));
+
+         that.owner.cameraPropUpdateReceivedFromServer(_result.system.cameralist[0].camera[index].number, props);
+      }
+
+      this.initialInfoReceived = true;
+      _callback(null, true);
+   });
 };
 
 module.exports = exports = SecuritySpyService;
