06b742dbfbd32d3fd77f950f76c0eee7a680f095
Richard Collin <richard@collin.me.uk>
2018-01-24 17:51:36 +0000

    fixed run script to deal with mulitple pis

--- run-casa.sh
+++ run-casa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export HOME="/home/pi"
-casa=${1}-config.json
+casa=${1}
 cd /home/pi/dev/casa/src
 
 RC=1
@@ -8,6 +8,6 @@ while [ $RC -ne 0 ]; do
    mv /home/pi/dev/casa/log.txt /home/pi/dev/casa/last-log.txt
    mv /home/pi/dev/casa/error.txt /home/pi/dev/casa/last-error.txt
    sudo -u pi git pull origin master
-   node app.js --system configs/casa-collin-config.json configs/env-config.json --secure >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
+   node app.js --system configs/casa-collin-config.json configs/${casa}-config.json --secure >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
    RC=$?
 done
