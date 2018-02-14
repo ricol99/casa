363fe52a46cf5c0013baac981e2169ccbd651185
Richard Collin <Richard.Collin@thomsonreuters.com>
2018-02-14 16:37:33 +0000

    bug

--- src/childcasa.js
+++ src/childcasa.js
@@ -2,11 +2,11 @@ var util = require('util');
 var PeerCasa = require('./peercasa');
 
 function ChildCasa(_config) {
+   _config.proActiveConnect = false;
    PeerCasa.call(this, _config);
    this.persistent = false;
 }
 
 util.inherits(ChildCasa, PeerCasa);
-
 
 module.exports = exports = ChildCasa;
