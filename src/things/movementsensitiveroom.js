b5729faef663df17c51d52f5472920035f0db361
Richard Collin <Richard.Collin@thomsonreuters.com>
2018-02-26 12:39:48 +0000

    config - movement sensitive room

---
+++ src/things/movementsensitiveroom.js
@@ -0,0 +1,48 @@
+var util = require('util');
+var Room = require('./room');
+
+// Please define properties for automated functionality
+// movement-pir - true when there is movement detected
+// low-light - true when light levels are low enough to switch on lights
+
+// Resulting user-state (s)
+// no-users-present - no movement detected
+// users-present - movement detected
+// users-detected-in-low-light - movement detected in low-light
+
+function MovementSensitiveRoom(_config) {
+
+   Room.call(this, _config);
+
+   this.movementTimeout = (_config.hasOwnProperty('movementTimeout')) ? _config.movementTimeout : 6000;
+
+   this.userStateConfig = {
+      "name": "user-state",
+      "type": "stateproperty",
+      "initialValue": "no-users-present",
+      "states": [
+         {  
+            "name": "no-users-present",
+            "sources": [ {  "property": "movement-pir", "value": true, "nextState": "users-present" } ],
+         },
+         {  
+            "name": "users-present",
+            "timeout": { "duration": this.movementTimeout, "nextState": "no-users-present" },
+            "sources": [ { "property": "low-light", "value": true, "nextState": "users-present-in-low-light" }, { "property": "movement-pir", "value": true, "nextState": "users-present" } ],
+         },
+         {
+            "name": "users-present-in-low-light",
+            "priority": 2,
+            "timeout": { "duration": this.movementTimeout, "nextState": "no-users-present" },
+            "sources": [ { "property": "movement-pir", "value": true, "nextState": "users-present-in-low-light" } ],
+         }
+      ]
+   };
+
+   this.ensurePropertyExists('user-state', 'stateproperty', this.userStateConfig, _config);
+}
+
+util.inherits(MovementSensitiveRoom, Room);
+
+
+module.exports = exports = MovementSensitiveRoom;
---
+++ src/utils/lightwave_util.sh
@@ -0,0 +1,42 @@
+#!/usr/local/bin/node
+
+var CasaSystem = require('../casasystem');
+casaSys = new CasaSystem({}, {"name": "casa:util", "displayName": "Utility Casa", "gang": "casa-collin-v2", "listeningPort": 8097}, false, false, null, null, 1);
+
+var LightWaveRF = require('../services/lightwaverfservice');
+var lrf = new LightWaveRF({ name: "lightwaverservice:utility", linkAddress: "192.168.1.49" });
+lrf.coldStart();
+
+var roomId = process.argv[2];
+var deviceId = process.argv[3];
+var action = process.argv[4];
+
+console.log('Performing action "'+action+'" on device '+deviceId+' in room '+roomId);
+
+switch (action) {
+   case "on":
+      lrf.turnDeviceOn(roomId, deviceId, (_err, _result) => {
+         if (_err) {
+            console.error('Error=',_err);
+            process.exit(1);
+         }
+         else {
+            console.info('Done');
+            process.exit(0);
+         }
+      });
+      break;
+
+   case "off":
+      lrf.turnDeviceOff(roomId, deviceId, (_err, _result) => {
+         if (_err) {
+            console.error('Error=',_err);
+            process.exit(1);
+         }
+         else {
+            console.info('Done');
+            process.exit(0);
+         }
+      });
+      break;
+}
