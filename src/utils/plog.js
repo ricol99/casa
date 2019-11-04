3c13330525eb5f8ee1dc84fac7843b9077a743b6
Richard Collin <richard@collin.me.uk>
2019-11-04 18:36:48 +0000

    console completion for plog

--- src/utils/plog.js
+++ src/utils/plog.js
@@ -7,6 +7,7 @@ for (var i=0; i < LOG.length-1; i++ ) {
 
    if (things.hasOwnProperty(LOG[i].uName)) {
      things[LOG[i].uName].props[LOG[i].prop] = LOG[i].value;
+      things[LOG[i].uName].timestamp = LOG[i].timestamp;
    }
    else {
       things[LOG[i].uName] = LOG[i];
@@ -24,7 +25,42 @@ else if (process.argv.length > 3) {
    console.log(things[process.argv[3]]);
 }
 else {
+   //for (var thing in things) {
+      //console.log(things[thing]);
+   //}
+
+   var str = "";
+
    for (var thing in things) {
-      console.log(things[thing]);
+      str += things[thing].uName + " ";
    }
+
+   const readline = require('readline');
+
+   var autoComplete = function completer(line) {
+      const completions = str.split(' ');
+      const hits = completions.filter((c) => c.startsWith(line));
+
+      return [hits.length ? hits : completions, line];
+   }
+
+
+   const rl = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout,
+     completer: autoComplete,
+     prompt: 'casa > '
+   });
+
+   rl.prompt();
+
+   rl.on('line', (line) => {
+     if (line === 'exit' || line ==='quit') {
+        process.exit(0);
+     }
+     console.log(things[line]);
+     rl.prompt();
+   }).on('close', () => {
+     process.exit(0);
+   });
 }
