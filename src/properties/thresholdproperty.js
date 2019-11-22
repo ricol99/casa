e7cac91d8f0710bc50593329711bea09dc9ebc1d
Richard Collin <richard@collin.me.uk>
2019-11-22 18:50:03 +0000

    new threshold property instead of using a step

--- src/properties/thresholdproperty.js
+++ src/properties/thresholdproperty.js
@@ -1,14 +1,49 @@
 var util = require('util');
-var SourceStepProperty = require('../steps/sourcestepproperty');
 
 function ThresholdProperty(_config, _owner) {
+   Property.call(this, _config, _owner);
 
-   _config.sourceStep = { type: 'thresholdstep', threshold: _config.threshold,
-                          thresholds: _config.thresholds, buffer: _config.buffer };
-
-   SourceStepProperty.call(this, _config, _owner);
+   // Thresholds with buffer must not overlap
+   this.thresholds = (_config.hasOwnProperty("threshold")) ? [_config.threshold] : _config.thresholds;
+   this.buffer = _config.buffer;
+   this.activeThreshold = -1;
 }
 
-util.inherits(ThresholdProperty, SourceStepProperty);
+util.inherits(ThresholdProperty, Property);
+
+ThresholdProperty.prototype.newEventReceivedFromSource = function(_sourceListener, _data) {
+   var newPropertyValue = _data.value;
+
+   if (this.cold) {
+      this.cold = false;
+   }
+   else {
+      if (this.activeThreshold == -1) {
+
+         for (var index = 0; index < this.thresholds.length; ++index) {
+
+            if (((this.value >= this.thresholds[index]) && (_data.value <= this.thresholds[index])) ||
+                ((this.value <= this.thresholds[index]) && (_data.value >= this.thresholds[index]))) {
+
+               this.activeThreshold = index;
+               break;
+            }
+         }
+      }
+
+      if (this.activeThreshold != -1) {
+
+         // We are currently buffering
+         if (Math.abs(_data.value - this.thresholds[this.activeThreshold]) > this.buffer) {
+            this.activeThreshold = -1;
+         }
+         else {
+            newPropertyValue = this.thresholds[this.activeThreshold];
+         }
+      }
+   }
+
+   this.updatePropertyInternal(newPropertyValue, _data);
+};
 
 module.exports = exports = ThresholdProperty;
