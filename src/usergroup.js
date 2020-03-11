999c01178e1f7d1a2f97b0b089858d5bed7716ca
Richard Collin <richard@collin.me.uk>
2020-03-11 11:04:06 +0000

    bug

--- src/usergroup.js
+++ src/usergroup.js
@@ -2,14 +2,13 @@ var util = require('util');
 var User = require('./user');
 
 function UserGroup(_config) {
+   User.call(this, _config);
+
    this.users = [];
 
    for (var u = 0; u < _config.users.length; ++u) {
       this.users.push(this.gang.findNamedObject(_config.users[u].fullName));
    }
-
-   User.call(this, _config);
-
 }
 
 util.inherits(UserGroup, User);
