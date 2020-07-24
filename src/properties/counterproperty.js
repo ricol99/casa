7d158b32beac4383071b3e307d6c152c7ee8b8d8
Richard Collin <richard@collin.me.uk>
2020-07-24 13:30:15 +0100

    adding simple counter property to aid example configs

---
+++ src/properties/counterproperty.js
@@ -0,0 +1,14 @@
+var util = require('util');
+var Property = require('../property');
+
+function CounterProperty(_config, _owner) {
+   Property.call(this, _config, _owner);
+}
+
+util.inherits(CounterProperty, Property);
+
+CounterProperty.prototype.newEventReceivedFromSource = function(_sourceListener, _data) {
+   this.updatePropertyInternal(this.value+1, _data);
+}
+
+module.exports = exports = CounterProperty;
