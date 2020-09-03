a3b15cf07943c80f0a29e9ae4b4581163609d8e3
Richard Collin <richard@collin.me.uk>
2020-09-03 14:29:17 +0100

    updated run-casa.sh to make it more configurable

--- run-casa.sh
+++ run-casa.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export HOME="/home/pi"
-casa=${1}
 cd /home/pi/dev/casa/src
 
 RC=1
@@ -8,6 +7,6 @@ while [ $RC -ne 0 ]; do
    mv /home/pi/dev/casa/log.txt /home/pi/dev/casa/last-log.txt
    mv /home/pi/dev/casa/error.txt /home/pi/dev/casa/last-error.txt
    sudo -u pi git pull origin master
-   node app.js ${casa} --secure >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
+   node app.js "$@" >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
    RC=$?
 done
