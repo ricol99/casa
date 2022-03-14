2a5a4171f3c59d177ff45b823aff840a28df43be
Richard Collin <richard@breakingwave.com>
2022-03-14 21:20:11 +0000

    Refactored touch switch - tested

--- src/things/touchswitch.js
+++ src/things/touchswitch.js
@@ -15,12 +15,6 @@ function TouchSwitch(_config, _parent) {
    this.invokeManualMode =  _config.hasOwnProperty("invokeManualMode") ? _config.invokeManualMode : !this.stateless;
    this.displayName = _config.displayName;
 
-   if (_config.hasOwnProperty("holdEventName")) {
-      this.holdEventName = _config.holdEventName;
-      this.holdTimerDuration = (_config.hasOwnProperty("holdTimerDuration")) ? _config.holdTimerDuration : 3;
-      this.holding = false;
-   }
-
    if (this.gpioFeedbackPin) {
       this.ensurePropertyExists(this.feedbackProp, 'gpioproperty', { initialValue: false, gpioPin: this.gpioFeedbackPin, direction: "out" }, _config);
    }
@@ -28,53 +22,48 @@ function TouchSwitch(_config, _parent) {
       this.ensurePropertyExists(this.feedbackProp, 'property', { initialValue: false }, _config);
    }
 
-   this.switchProp = _config.hasOwnProperty("switchProp") ? _config.switchProp : "switch-state";
+   this.switchProp = _config.hasOwnProperty("switchProp") ? _config.switchProp : "switch-active";
 
    this.ensurePropertyExists(this.switchProp, 'gpioproperty', { initialValue: false, gpioPin: this.gpioTouchPin, triggerLow: this.triggerLow }, _config);
+
+   this.holdStartEventName =  _config.hasOwnProperty("holdStartEventName") ? _config.holdStartEventName : "touch-hold-start-event";
+   this.holdConfirmEventName =  _config.hasOwnProperty("holdConfirmEventName") ? _config.holdConfirmEventName : "touch-hold-confirm-event";
+   this.holdStartTimerDuration = (_config.hasOwnProperty("holdStartTimerDuration")) ? _config.holdStartTimerDuration : 1;
+   this.holdTimerDuration = (_config.hasOwnProperty("holdTimerDuration")) ? _config.holdTimerDuration : 2;
+
+   this.ensurePropertyExists("hold-switch-event", "property", { initialValue: _config.hasOwnProperty("holdSwitchEvent") ? _config.holdSwitchEvent : false }, _config);
+
+   this.ensurePropertyExists("switch-state", "stateproperty", { initialValue: "idle", type: "stateproperty", ignoreControl: true, takeControlOnTransition: true,
+                                                                states: [{ name: "idle",
+                                                                           sources: [{ property: "hold-switch-event", value: true, nextState: "idle-hold-switch-event" },
+                                                                                     { property: this.switchProp, value: true, nextState: "user-touching" } ]},
+                                                                         { name: "idle-hold-switch-event",
+                                                                           sources: [{ property: this.switchProp, value: true, nextState: "user-touching-hold-switch-event" },
+                                                                                     { property: "hold-switch-event", value: false, nextState: "idle" }]},
+                                                                         { name: "user-touching", actions: [{ event: this.eventName }],
+                                                                           timeout: { duration: this.holdStartTimerDuration, nextState: "user-holding" },
+                                                                           sources: [{ property: this.switchProp, value: false, nextState: "idle"}]},
+                                                                         { name: "user-touching-hold-switch-event",
+                                                                           timeout: { duration: this.holdStartTimerDuration, nextState: "user-holding-hold-switch-event" },
+                                                                           sources: [{ property: this.switchProp, value: false, nextState: "send-delayed-touch-event"}]},
+                                                                         { name: "send-delayed-touch-event", actions: [{ event: this.eventName }],
+                                                                           timeout: { duration: 0.1, nextState: "idle" }},
+                                                                         { name: "user-holding", actions: [{ event: this.holdStartEventName }],
+                                                                           timeout: { duration: this.holdTimerDuration, nextState: "user-hold-confirmed" },
+                                                                           sources: [{ property: this.switchProp, value: false, nextState: "idle"}]},
+                                                                         { name: "user-holding-hold-switch-event", actions: [{ event: this.holdStartEventName }],
+                                                                           timeout: { duration: this.holdTimerDuration, nextState: "user-hold-confirmed" },
+                                                                           sources: [{ property: this.switchProp, value: false, nextState: "send-delayed-touch-event"}]},
+                                                                         { name: "user-hold-confirmed", actions: [{ event: this.holdConfirmEventName }],
+                                                                           sources: [{ property: this.switchProp, value: false, nextState: "idle"}]} ]}, _config);
 }
 
 util.inherits(TouchSwitch, Thing);
 
 TouchSwitch.prototype.propertyAboutToChange = function(_propName, _propValue, _data) {
 
-   if (!_data.coldStart) {
-
-      if (_propName === this.switchProp) {
-
-         if (this.stateless) {
-
-            if (_propValue) {
-
-               if (this.holdEventName) {
-
-                  this.holdTimer = setTimeout( () => {
-                     this.holdTimer = null;
-                     this.raiseEvent(this.holdEventName);
-                     this.raiseEvent(this.holdEventName+"-start");
-                     this.holding = true;
-                  }, this.holdTimerDuration * 1000);
-               }
-               else {
-                  this.raiseEvent(this.eventName);
-               }
-            }
-            else if (this.holdEventName) {
-
-               if (this.holdTimer) {
-                  clearTimeout(this.holdTimer);
-                  this.holdTimer = null;
-                  this.raiseEvent(this.eventName);
-               }
-               else if (this.holding) {
-                  this.holding = false;
-                  this.raiseEvent(this.holdEventName+"-end");
-               }
-            }
-         }
-         else if (this.invokeManualMode) {
-            this.setManualMode();
-         }
-      }
+   if (!_data.coldStart && (_propName === this.switchProp) && _propValue && this.invokeManualMode) {
+      this.setManualMode();
    }
 };
 
