9e5af87fdba15d46cda88ac8b6f22acad8689067
Richard Collin <richard@collin.me.uk>
2025-05-09 09:33:46 +0100

    git sync command update

--- run-casa.sh
+++ run-casa.sh
@@ -6,7 +6,9 @@ RC=1
 while [ $RC -ne 0 ]; do
    mv /home/pi/dev/casa/log.txt /home/pi/dev/casa/last-log.txt
    mv /home/pi/dev/casa/error.txt /home/pi/dev/casa/last-error.txt
-   sudo -u pi git pull origin master
+   sudo -u pi git stash
+   sudo -u pi git pull --rebase origin master
+   sudo -u pi git stash apply
    node app.js "$@" >/home/pi/dev/casa/log.txt 2>/home/pi/dev/casa/error.txt
    RC=$?
 done
