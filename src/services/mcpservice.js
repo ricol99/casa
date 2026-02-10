ee7eada6be30e296a3335a26f960543544899654
Richard Collin <richard@collin.me.uk>
2026-02-10 10:57:44 +0000

    Align MCP protocol and auth behaviors

--- src/services/mcpservice.js
+++ src/services/mcpservice.js
@@ -1,6 +1,8 @@
 var util = require('util');
 var WebService = require('./webservice');
 
+var MCP_PROTOCOL_VERSION = "2025-06-18";
+
 function McpService(_config, _owner) {
    _config.socketIoSupported = false;
    WebService.call(this, _config, _owner);
@@ -8,6 +10,9 @@ function McpService(_config, _owner) {
    this.mcpRoute = _config.hasOwnProperty("mcpRoute") ? _config.mcpRoute : "/mcp";
    this.maxBodyBytes = _config.hasOwnProperty("maxBodyBytes") ? _config.maxBodyBytes : 1024 * 1024;
    this.auth = _config.hasOwnProperty("auth") ? _config.auth : { mode: "none" };
+   this.supportedProtocolVersions = _config.hasOwnProperty("supportedProtocolVersions")
+      ? _config.supportedProtocolVersions
+      : [ MCP_PROTOCOL_VERSION ];
 }
 
 util.inherits(McpService, WebService);
@@ -33,6 +38,12 @@ McpService.prototype.hotStart = function() {
 McpService.prototype.start = function() {
    WebService.prototype.start.call(this);
 
+   this.addRoute("/.well-known/oauth-protected-resource", McpService.prototype.handleProtectedResourceMetadata.bind(this));
+
+   if (this.mcpRoute !== "/") {
+      this.addRoute("/.well-known/oauth-protected-resource" + this.mcpRoute, McpService.prototype.handleProtectedResourceMetadata.bind(this));
+   }
+
    this.addRoute(this.mcpRoute, McpService.prototype.handleGet.bind(this));
    this.addRoute(this.mcpRoute + "/.well-known/oauth-authorization-server", McpService.prototype.handleOauthMetadata.bind(this));
    this.addRoute(this.mcpRoute + "/oauth", McpService.prototype.handleOauthPlaceholder.bind(this));
@@ -40,8 +51,9 @@ McpService.prototype.start = function() {
 };
 
 McpService.prototype.handleGet = function(_request, _response) {
-   if (!this.isAuthorized(_request)) {
-      return this.sendJsonRpcError(_response, null, -32001, "Unauthorized");
+
+   if (!this.isAuthorized(_request) && !(this.auth && this.auth.publicGet)) {
+      return this.sendUnauthorized(_request, _response);
    }
 
    _response.status(200).json({
@@ -53,7 +65,7 @@ McpService.prototype.handleGet = function(_request, _response) {
 McpService.prototype.handlePost = function(_request, _response) {
 
    if (!this.isAuthorized(_request)) {
-      return this.sendJsonRpcError(_response, null, -32001, "Unauthorized");
+      return this.sendUnauthorized(_request, _response);
    }
 
    this.collectJsonBody(_request, (_err, _body) => {
@@ -61,15 +73,11 @@ McpService.prototype.handlePost = function(_request, _response) {
          return this.sendJsonRpcError(_response, null, -32700, "Invalid JSON");
       }
 
-      return this.handleJsonRpc(_body, _response);
+      return this.handleJsonRpc(_body, _request, _response);
    });
 };
 
 McpService.prototype.handleOauthMetadata = function(_request, _response) {
-   if (!this.isAuthorized(_request)) {
-      return this.sendJsonRpcError(_response, null, -32001, "Unauthorized");
-   }
-
    var baseUrl = this.getBaseUrl(_request);
    var issuer = this.auth && this.auth.issuer ? this.auth.issuer : baseUrl;
 
@@ -86,13 +94,19 @@ McpService.prototype.handleOauthMetadata = function(_request, _response) {
 };
 
 McpService.prototype.handleOauthPlaceholder = function(_request, _response) {
-   if (!this.isAuthorized(_request)) {
-      return this.sendJsonRpcError(_response, null, -32001, "Unauthorized");
-   }
-
    _response.status(501).json({
       error: "not_implemented",
       message: "OAuth endpoints are placeholders. Configure auth.issuer and proxy to your OAuth provider."
+   });
+};
+
+McpService.prototype.handleProtectedResourceMetadata = function(_request, _response) {
+   var issuer = this.auth && this.auth.issuer ? this.auth.issuer : this.getBaseUrl(_request);
+   var resource = this.getBaseUrl(_request) + this.mcpRoute;
+
+   _response.status(200).json({
+      resource: resource,
+      authorization_servers: [ issuer ]
    });
 };
 
@@ -156,6 +170,15 @@ McpService.prototype.isAuthorized = function(_request) {
    return false;
 };
 
+McpService.prototype.sendUnauthorized = function(_request, _response) {
+   var resourceMetadataUrl = this.getBaseUrl(_request) + "/.well-known/oauth-protected-resource";
+
+   _response.set("WWW-Authenticate", "Bearer resource_metadata=\"" + resourceMetadataUrl + "\"");
+   _response.status(401).json({
+      error: "unauthorized"
+   });
+};
+
 McpService.prototype.getBaseUrl = function(_request) {
    var proto = _request.headers && _request.headers["x-forwarded-proto"]
       ? _request.headers["x-forwarded-proto"]
@@ -166,26 +189,55 @@ McpService.prototype.getBaseUrl = function(_request) {
    return proto + "://" + host;
 };
 
-McpService.prototype.handleJsonRpc = function(_body, _response) {
+McpService.prototype.handleJsonRpc = function(_body, _request, _response) {
 
    if (!_body || !_body.method) {
       return this.sendJsonRpcError(_response, null, -32600, "Invalid Request");
+   }
+
+   if (_body instanceof Array) {
+      return this.sendJsonRpcError(_response, null, -32600, "Batch requests are not supported");
    }
 
    var id = _body.hasOwnProperty("id") ? _body.id : null;
    var method = _body.method;
    var params = _body.params;
 
-   // TODO: Align method names and response format with the MCP specification.
    if (method === "initialize") {
+      var requestedVersion = params && params.protocolVersion ? params.protocolVersion : MCP_PROTOCOL_VERSION;
+      var negotiatedVersion = this.supportedProtocolVersions.indexOf(requestedVersion) !== -1
+         ? requestedVersion
+         : MCP_PROTOCOL_VERSION;
+
+      this.setProtocolHeader(_response, negotiatedVersion);
+
       return this.sendJsonRpcResult(_response, id, {
-         name: "casa-mcp",
-         version: "0.1.0",
+         protocolVersion: negotiatedVersion,
          capabilities: {
-            tools: true,
-            resources: false
+            tools: {
+               listChanged: false
+            }
+         },
+         serverInfo: {
+            name: "casa-mcp",
+            version: "0.1.0"
          }
       });
+   }
+
+   if (!this.hasProtocolHeader(_request)) {
+      return this.sendJsonRpcError(_response, id, -32600, "Missing MCP-Protocol-Version header");
+   }
+
+   this.setProtocolHeader(_response, _request.headers["mcp-protocol-version"]);
+
+   if (!id && method && method.startsWith("notifications/")) {
+      _response.status(204).end();
+      return null;
+   }
+
+   if (method === "ping") {
+      return this.sendJsonRpcResult(_response, id, {});
    }
 
    if (method === "tools/list") {
@@ -193,16 +245,21 @@ McpService.prototype.handleJsonRpc = function(_body, _response) {
          tools: [
             {
                name: "casa.get_named_object_tree",
+               title: "Get Named Object Tree",
                description: "Return an exported named-object subtree",
                inputSchema: {
                   type: "object",
                   properties: {
                      uName: { type: "string" }
                   }
+               },
+               annotations: {
+                  readOnlyHint: true
                }
             },
             {
                name: "casa.set_mode_manual",
+               title: "Set Manual Mode",
                description: "Set MODE to manual for a named object",
                inputSchema: {
                   type: "object",
@@ -211,6 +268,9 @@ McpService.prototype.handleJsonRpc = function(_body, _response) {
                      duration: { type: "number" }
                   },
                   required: [ "uName" ]
+               },
+               annotations: {
+                  readOnlyHint: false
                }
             }
          ]
@@ -241,7 +301,11 @@ McpService.prototype.handleToolCall = function(_id, _params, _response) {
       var exportObj = {};
       obj.export(exportObj);
 
-      return this.sendJsonRpcResult(_response, _id, { content: exportObj });
+      return this.sendJsonRpcResult(_response, _id, {
+         content: [ { type: "text", text: JSON.stringify(exportObj) } ],
+         structuredContent: exportObj,
+         isError: false
+      });
    }
 
    if (_params.name === "casa.set_mode_manual") {
@@ -259,7 +323,11 @@ McpService.prototype.handleToolCall = function(_id, _params, _response) {
          target.setManualMode();
       }
 
-      return this.sendJsonRpcResult(_response, _id, { ok: true });
+      return this.sendJsonRpcResult(_response, _id, {
+         content: [ { type: "text", text: "ok" } ],
+         structuredContent: { ok: true },
+         isError: false
+      });
    }
 
    return this.sendJsonRpcError(_response, _id, -32601, "Unknown tool");
@@ -284,6 +352,17 @@ McpService.prototype.sendJsonRpcError = function(_response, _id, _code, _message
          message: _message
       }
    });
+};
+
+McpService.prototype.hasProtocolHeader = function(_request) {
+   var header = _request && _request.headers ? _request.headers["mcp-protocol-version"] : null;
+   return !!header;
+};
+
+McpService.prototype.setProtocolHeader = function(_response, _version) {
+   if (_version) {
+      _response.set("MCP-Protocol-Version", _version);
+   }
 };
 
 module.exports = exports = McpService;
