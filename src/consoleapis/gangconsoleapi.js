c62254b2dc69ee961238e005acddd68691864d33
Richard Collin <richard@collin.me.uk>
2026-03-05 15:12:32 +0000

    testing topology and resolveSource commands

--- src/consoleapis/gangconsoleapi.js
+++ src/consoleapis/gangconsoleapi.js
@@ -70,24 +70,108 @@ function getSourceScope(_source, _gangName, _localCasaName) {
    return "runtime";
 }
 
-function collectSourceCounts(_sources) {
-   var counts = { total: 0, bowed: 0, active: 0 };
+function getObjectType(_obj) {
 
-   if (!_sources) {
+   if (!_obj) {
+      return "unknown";
+   }
+
+   if (_obj.type) {
+      return _obj.type;
+   }
+
+   if (_obj.config && _obj.config.type) {
+      return _obj.config.type;
+   }
+
+   if (typeof _obj.superType === "function") {
+      return _obj.superType();
+   }
+
+   return "unknown";
+}
+
+function findInstanceMeta(_instances, _source) {
+
+   if (!_source) {
+      return -1;
+   }
+
+   for (var i = 0; i < _instances.length; ++i) {
+
+      if (_instances[i].source === _source) {
+         return i;
+      }
+   }
+
+   return -1;
+}
+
+function appendSourcesFromMap(_instances, _sourceMap, _sourceUName, _inSources, _inBowing) {
+
+   if (!_sourceMap) {
+      return;
+   }
+
+   if (_sourceUName) {
+
+      if (_sourceMap.hasOwnProperty(_sourceUName) && _sourceMap[_sourceUName]) {
+         var existingIndex = findInstanceMeta(_instances, _sourceMap[_sourceUName]);
+
+         if (existingIndex === -1) {
+            _instances.push({ source: _sourceMap[_sourceUName], inSources: !!_inSources, inBowing: !!_inBowing });
+         }
+         else {
+            _instances[existingIndex].inSources = _instances[existingIndex].inSources || !!_inSources;
+            _instances[existingIndex].inBowing = _instances[existingIndex].inBowing || !!_inBowing;
+         }
+      }
+   }
+   else {
+      for (var sourceName in _sourceMap) {
+
+         if (_sourceMap.hasOwnProperty(sourceName) && _sourceMap[sourceName]) {
+            var existingIndex2 = findInstanceMeta(_instances, _sourceMap[sourceName]);
+
+            if (existingIndex2 === -1) {
+               _instances.push({ source: _sourceMap[sourceName], inSources: !!_inSources, inBowing: !!_inBowing });
+            }
+            else {
+               _instances[existingIndex2].inSources = _instances[existingIndex2].inSources || !!_inSources;
+               _instances[existingIndex2].inBowing = _instances[existingIndex2].inBowing || !!_inBowing;
+            }
+         }
+      }
+   }
+}
+
+function appendContainerInstances(_instances, _container, _sourceUName) {
+
+   if (!_container) {
+      return;
+   }
+
+   appendSourcesFromMap(_instances, _container.sources, _sourceUName, true, false);
+   appendSourcesFromMap(_instances, _container.bowingSources, _sourceUName, false, true);
+}
+
+function collectSourceCounts(_container) {
+   var counts = { total: 0, bowed: 0, active: 0 };
+   var metas = [];
+
+   if (!_container) {
       return counts;
    }
 
-   for (var sourceUName in _sources) {
+   appendContainerInstances(metas, _container);
+   counts.total = metas.length;
 
-      if (_sources.hasOwnProperty(sourceUName) && _sources[sourceUName]) {
-         ++counts.total;
-
-         if (_sources[sourceUName].bowing) {
-            ++counts.bowed;
-         }
-         else {
-            ++counts.active;
-         }
+   for (var i = 0; i < metas.length; ++i) {
+      if (metas[i].inBowing) {
+         ++counts.bowed;
+      }
+      else if (metas[i].inSources) {
+         ++counts.active;
       }
    }
 
@@ -195,14 +279,14 @@ GangConsoleApi.prototype.exportDb = function(_session, _params, _callback) {
 };
 
 GangConsoleApi.prototype.topology = function(_session, _params, _callback) {
-   var localCounts = collectSourceCounts(this.gang.casa.sources);
+   var localCounts = collectSourceCounts(this.gang.casa);
    var peers = [];
 
    for (var peerName in this.gang.peercasas) {
 
       if (this.gang.peercasas.hasOwnProperty(peerName)) {
          var peerCasa = this.gang.peercasas[peerName];
-         var peerCounts = collectSourceCounts(peerCasa.sources);
+         var peerCounts = collectSourceCounts(peerCasa);
 
          peers.push({
             casaName: peerCasa.name,
@@ -229,46 +313,41 @@ GangConsoleApi.prototype.topology = function(_session, _params, _callback) {
 
 GangConsoleApi.prototype.resolveSourceInternal = function(_sourceUName) {
    var activeSource = null;
-   var instances = [];
+   var instanceMetas = [];
    var activeOwnerCasa = null;
    var activeProviderType = null;
    var localCasaName = this.gang.casa.name;
 
-   if (this.gang.casa.sources && this.gang.casa.sources.hasOwnProperty(_sourceUName) && this.gang.casa.sources[_sourceUName]) {
-      instances.push(this.gang.casa.sources[_sourceUName]);
-   }
+   appendContainerInstances(instanceMetas, this.gang.casa, _sourceUName);
 
    for (var peerName in this.gang.peercasas) {
 
       if (this.gang.peercasas.hasOwnProperty(peerName)) {
          var peerCasa = this.gang.peercasas[peerName];
-
-         if (peerCasa.sources && peerCasa.sources.hasOwnProperty(_sourceUName) && peerCasa.sources[_sourceUName]) {
-            instances.push(peerCasa.sources[_sourceUName]);
-         }
+         appendContainerInstances(instanceMetas, peerCasa, _sourceUName);
       }
    }
 
-   if (instances.length > 0) {
+   if (instanceMetas.length > 0) {
       activeSource = this.gang.findNamedObject(_sourceUName);
    }
 
    var outputInstances = [];
 
-   for (var i = 0; i < instances.length; ++i) {
-      var source = instances[i];
+   for (var i = 0; i < instanceMetas.length; ++i) {
+      var source = instanceMetas[i].source;
       var ownerCasa = getSourceOwnerCasa(source, localCasaName);
       var providerType = getSourceProviderType(source);
       var connected = (providerType === "peercasa") ? !!(source.casa && source.casa.connected) : true;
       var state = "standby";
 
-      if (activeSource && (source === activeSource) && !source.bowing) {
+      if (instanceMetas[i].inBowing) {
+         state = "bowed";
+      }
+      else if (activeSource && (source === activeSource) && !source.bowing) {
          state = "active";
          activeOwnerCasa = ownerCasa;
          activeProviderType = providerType;
-      }
-      else if (source.bowing) {
-         state = "bowed";
       }
       else if (!connected) {
          state = "unavailable";
@@ -277,7 +356,8 @@ GangConsoleApi.prototype.resolveSourceInternal = function(_sourceUName) {
       outputInstances.push({
          ownerCasa: ownerCasa,
          providerType: providerType,
-         sourceType: (typeof source.superType === "function") ? source.superType() : "unknown",
+         type: getObjectType(source),
+         superType: (typeof source.superType === "function") ? source.superType() : null,
          priority: (source.priority !== undefined) ? source.priority : 0,
          state: state,
          connected: connected,
@@ -292,7 +372,8 @@ GangConsoleApi.prototype.resolveSourceInternal = function(_sourceUName) {
       outputInstances.push({
          ownerCasa: activeOwnerCasa,
          providerType: activeProviderType,
-         sourceType: (typeof activeSource.superType === "function") ? activeSource.superType() : "unknown",
+         type: getObjectType(activeSource),
+         superType: (typeof activeSource.superType === "function") ? activeSource.superType() : null,
          priority: (activeSource.priority !== undefined) ? activeSource.priority : 0,
          state: "active",
          connected: true,
